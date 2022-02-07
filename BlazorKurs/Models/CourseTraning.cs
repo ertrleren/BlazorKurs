7eefc127c0a532cc0ed25c0e0d01c82e399c5b8a
Ertuğrul Eren <ertrleren@gmail.com>
2022-02-07 12:01:08 +0300

    .

---
+++ BlazorKurs/Models/CourseTraning.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorKurs.Models
+{
+    public class CourseTraning
+    {
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+    
+}
